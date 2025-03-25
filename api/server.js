3bf9081649195cb302239dea0e9f113f02b25b11
Rebecaalimaa <rebecalimanascimento3@gmail.com>
2025-03-25 16:02:11 -0300

    terminei

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const router = require('./src/router');
+const router = require('./src/routes');
 
 app.use(cors());
 app.use(express.json());
